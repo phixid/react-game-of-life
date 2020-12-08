dd32ee4269bb783c789eca8110a408c8bd0d70bf
Kristof Hermans <kristof.hermans@nike.com>
2020-12-08 16:27:26 +0100

    set up the base colony / tribe / individual and their styling

---
+++ src/components/Individual.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import {borderColor} from "../constants";
+
+const Individual = ({individual = 0}) => <StyledIndividual alive={individual === 1} />;
+
+const aliveColor = '#9fcfff';
+const size = '2rem';
+const StyledIndividual = styled('div')`
+  background: ${ props => props.alive ? aliveColor : '#ffffff' };
+  border-right: 1px solid ${ borderColor };
+  display: flex;
+  flex: 1 1 0;
+  flex-direction: row;
+  height: ${ size };
+  width: ${ size };
+`;
+
+export default Individual;
---
+++ src/components/Tribe.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import Individual from "./Individual";
+import {borderColor} from "../constants";
+
+const Tribe = ({ tribe = [] }) => {
+    return <StyledTribe>
+        { tribe.map((individual, i) => <Individual individual={ individual } key={i} />) }
+    </StyledTribe>;
+};
+
+const StyledTribe = styled('div')`
+  border-bottom: 1px solid ${ borderColor };
+  display: flex;
+  flex-direction: row;
+`;
+
+export default Tribe;
